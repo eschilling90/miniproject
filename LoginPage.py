d4a7d9715a2ee0a357572df55b5f95918247b721
eschilling90 <erik.schilling@gmail.com>
2014-09-15 00:01:04 -0500

    Updated API to include data storage

--- LoginPage.py
+++ LoginPage.py
@@ -4,6 +4,7 @@ import urllib
 from google.appengine.api import users
 from google.appengine.ext import ndb
 
+import connexus
 import jinja2
 import webapp2
 
@@ -12,7 +13,7 @@ JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
-	
+
 class LoginPage(webapp2.RequestHandler):
 
     def get(self):
@@ -23,12 +24,18 @@ class LoginPage(webapp2.RequestHandler):
         else:
             url = users.create_login_url(self.request.uri)
             url_linktext = 'Login'
-
+        connexus.addNewUser(3, "user3")
+        userList, t = connexus.getUserStreams("user3")
         template_values = {
+            'users': t + userList,
+            'url_linktext': connexus.userCount()
         }
 
         template = JINJA_ENVIRONMENT.get_template('index.html')
         self.response.write(template.render(template_values))
+
+    def adduser(self):
+        connexus.addNewUser(connexus.getNewUserId(), "user" + str(connexus.getNewUserId()))
 
 application = webapp2.WSGIApplication([
     ('/', LoginPage),
